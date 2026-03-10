3f93d895a83232024a5f5ab625e5106d54170bf9
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-03-10 10:06:00 +0100

    plone.meta

--- setup.py
+++ setup.py
@@ -1,52 +1,49 @@
 from setuptools import setup, find_packages
 
 
-version = '1.7.3.dev0'
+version = "1.7.3.dev0"
 
 setup(
-    name='collective.geolocationbehavior',
+    name="collective.geolocationbehavior",
     version=version,
     description="Dexterity behavior to add geographic locations to contents.",
     long_description="{0}\n{1}".format(
-        open("README.rst").read(),
-        open("CHANGES.rst").read()
+        open("README.rst").read(), open("CHANGES.rst").read()
     ),
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
-        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.2",
         "Framework :: Plone :: Addon",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='plone dexterity behavior geographic locations contents',
-    author='Jesse Snyder, davisagli et al',
-    url='https://github.com/collective/collective.geolocationbehavior/',
-    license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['collective'],
+    keywords="plone dexterity behavior geographic locations contents",
+    author="Jesse Snyder, davisagli et al",
+    url="https://github.com/collective/collective.geolocationbehavior/",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["collective"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
+        "setuptools",
         # Dependencies for the behavior and form
-        'plone.app.dexterity',  # remove this one to get a generic behavior
-        'plone.autoform',
-        'plone.behavior',
-        'plone.dexterity',
-        'plone.formwidget.geolocation',
-        'plone.supermodel',
+        "plone.app.dexterity",  # remove this one to get a generic behavior
+        "plone.autoform",
+        "plone.behavior",
+        "plone.dexterity",
+        "plone.formwidget.geolocation",
+        "plone.supermodel",
         # Framework dependencies
-        'zope.annotation',
-        'zope.component',
-        'zope.interface',
+        "zope.annotation",
+        "zope.component",
+        "zope.interface",
     ],
     entry_points="""
       [z3c.autoinclude.plugin]
